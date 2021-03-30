ee6bf7c1b8344593d16025c6aecf2d71543f4532
mbimbij <joseph.mbimbi@gmail.com>
2021-03-30 09:52:05 +0200

    Add a manual approval stage to the pipeline

--- create-pipeline.sh
+++ create-pipeline.sh
@@ -10,9 +10,9 @@ testAccountId=$(getAccountId $ACCOUNT_TEST_PROFILE)
 applicationName=serverless-cicd
 s3BucketName="$applicationName-artifacts-bucket"
 
-############################################################
+echo "############################################################"
 echo "deploying s3 artifacts bucket and kms key"
-############################################################
+echo "############################################################"
 aws cloudformation deploy \
   --stack-name $applicationName-pre-requesites-stack \
   --template-file pre-requesites.yml \
@@ -22,19 +22,19 @@ aws cloudformation deploy \
 
 s3KmsKeyArn=$(aws cloudformation describe-stacks --stack-name $applicationName-pre-requesites-stack --profile $ACCOUNT_OPERATIONS_PROFILE --query "Stacks[*].Outputs[?OutputKey=='CMK'].OutputValue" --output text)
 
-############################################################
+echo "############################################################"
 echo "deploying IAM roles in test account"
-############################################################
+echo "############################################################"
 aws cloudformation deploy \
   --stack-name $applicationName-roles-stack \
-  --template-file pipeline-setup-cross-accont-roles.yml \
+  --template-file pipeline-cross-account-deploy-stack.yml \
   --capabilities CAPABILITY_NAMED_IAM \
   --profile $ACCOUNT_TEST_PROFILE \
   --parameter-overrides OpsAccountId=$operationsAccountId S3BucketName=$s3BucketName S3KmsKeyArn=$s3KmsKeyArn
 
-############################################################
+echo "############################################################"
 echo "deploying pipeline in ops account"
-############################################################
+echo "############################################################"
 aws cloudformation deploy \
   --stack-name $applicationName-pipeline-stack \
   --template-file pipeline-ops-account-stack.yml \
@@ -42,7 +42,9 @@ aws cloudformation deploy \
   --profile $ACCOUNT_OPERATIONS_PROFILE \
   --parameter-overrides ApplicationName=$applicationName TestAccountId=$testAccountId S3BucketName=$s3BucketName S3KmsKeyArn=$s3KmsKeyArn
 
+echo "############################################################"
 echo "updating artifacts bucket kms key policy for cross-account access"
+echo "############################################################"
 aws cloudformation deploy \
   --stack-name $applicationName-pre-requesites-stack \
   --template-file pre-requesites.yml \
