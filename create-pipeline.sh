cd2b14415d4afecda105b22f1c8c8e428d634c5b
mbimbij <joseph.mbimbi@gmail.com>
2021-04-11 23:19:05 +0200

    Remove hardcoded 'application-name'

--- create-pipeline.sh
+++ create-pipeline.sh
@@ -4,17 +4,22 @@ getAccountId () {
   aws sts get-caller-identity --profile $1 --query "Account" --output text
 }
 
+if [ -z $1 ]; then
+  echo -e "usage:\n./create-pipeline.sh \$APPLICATION_NAME"
+  exit 1
+fi
+
+applicationName=$1
+
 operationsAccountId=$(getAccountId $ACCOUNT_OPERATIONS_PROFILE)
 echo "operationsAccountId: $operationsAccountId"
 testAccountId=$(getAccountId $ACCOUNT_TEST_PROFILE)
 echo "testAccountId: $testAccountId"
 prodAccountId=$(getAccountId $ACCOUNT_PROD_PROFILE)
 echo "prodAccountId: $prodAccountId"
-echo "prodAccountId: $GITHUB_REPO"
-
-
-applicationName=serverless-cicd
-s3BucketName="$applicationName-artifacts-bucket"
+echo "github repo: $GITHUB_REPO"
+s3BucketName="$operationsAccountId-$AWS_REGION-$applicationName-artifacts-bucket"
+echo "s3BucketName: $s3BucketName"
 
 echo "############################################################"
 echo "deploying s3 artifacts bucket and kms key"
@@ -27,6 +32,7 @@ aws cloudformation deploy \
   --parameter-overrides ApplicationName=$applicationName S3BucketName=$s3BucketName TestAccountId=$testAccountId ProdAccountId=$prodAccountId
 
 s3KmsKeyArn=$(aws cloudformation describe-stacks --stack-name $applicationName-pre-requesites-stack --profile $ACCOUNT_OPERATIONS_PROFILE --query "Stacks[*].Outputs[?OutputKey=='CMK'].OutputValue" --output text)
+echo "s3KmsKeyArn: $s3KmsKeyArn"
 
 echo "############################################################"
 echo "deploying IAM roles in test account"
@@ -36,7 +42,7 @@ aws cloudformation deploy \
   --template-file pipeline-cross-account-deploy-stack.yml \
   --capabilities CAPABILITY_NAMED_IAM \
   --profile $ACCOUNT_TEST_PROFILE \
-  --parameter-overrides OpsAccountId=$operationsAccountId S3BucketName=$s3BucketName S3KmsKeyArn=$s3KmsKeyArn
+  --parameter-overrides ApplicationName=$applicationName OpsAccountId=$operationsAccountId S3BucketName=$s3BucketName S3KmsKeyArn=$s3KmsKeyArn
 
 echo "############################################################"
 echo "deploying IAM roles in prod account"
@@ -46,17 +52,18 @@ aws cloudformation deploy \
   --template-file pipeline-cross-account-deploy-stack.yml \
   --capabilities CAPABILITY_NAMED_IAM \
   --profile $ACCOUNT_PROD_PROFILE \
-  --parameter-overrides OpsAccountId=$operationsAccountId S3BucketName=$s3BucketName S3KmsKeyArn=$s3KmsKeyArn
+  --parameter-overrides ApplicationName=$applicationName OpsAccountId=$operationsAccountId S3BucketName=$s3BucketName S3KmsKeyArn=$s3KmsKeyArn
 
 echo "############################################################"
 echo "deploying pipeline in ops account"
 echo "############################################################"
+samStackName=$applicationName-sam-stack
 aws cloudformation deploy \
   --stack-name $applicationName-pipeline-stack \
   --template-file pipeline-ops-account-stack.yml \
   --capabilities CAPABILITY_NAMED_IAM \
   --profile $ACCOUNT_OPERATIONS_PROFILE \
-  --parameter-overrides ApplicationName=$applicationName S3BucketName=$s3BucketName S3KmsKeyArn=$s3KmsKeyArn TestAccountId=$testAccountId ProdAccountId=$prodAccountId GithubRepo=$GITHUB_REPO
+  --parameter-overrides ApplicationName=$applicationName SamStackName=$samStackName S3BucketName=$s3BucketName S3KmsKeyArn=$s3KmsKeyArn TestAccountId=$testAccountId ProdAccountId=$prodAccountId GithubRepo=$GITHUB_REPO
 
 echo "############################################################"
 echo "updating artifacts bucket kms key policy for cross-account access"
